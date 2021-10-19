d4348f648519c57ca121e443f14ae55d9be5a9a7
shiba_dog <shibadog99@gmail.com>
2021-10-19 15:08:59 +0900

    ittan

--- run.ps1
+++ run.ps1
@@ -1,6 +1,8 @@
-$ipaddress = [system.net.dns]::GetHostAddresses((hostname)) | `
-    Where-Object { $_.AddressFamily -eq "InterNetwork" } | `
-    Select-Object -ExpandProperty IPAddressToString -First 1
+# $ipaddress = [system.net.dns]::GetHostAddresses((hostname)) | `
+#     Where-Object { $_.AddressFamily -eq "InterNetwork" } | `
+#     Select-Object -ExpandProperty IPAddressToString -First 1
+
+$ipaddress = hostname
 
 $env:CONCOURSE_EXTERNAL_URL = "http://${ipaddress}:8080"
 Write-Output $env:CONCOURSE_EXTERNAL_URL
